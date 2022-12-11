01f697e63b171e116f8f17e12f1abb76a04d75dc
Yousuf Islam Rathore <rathoreislam@gmail.com>
2022-12-11 23:09:07 +0500

    Updating UI

---
+++ assets/scripts/index.js
@@ -0,0 +1,14 @@
+function signUp2StepsToggler(redirectorSection, key) {
+  const signerMessage = document.getElementById("signerMessage");
+  const signUp2Btn = document.getElementById("signUp2Btn");
+  redirectorSection.className = "active-redirector";
+  if (key == 0) {
+    redirectorSection.nextElementSibling.className = "";
+    signerMessage.firstElementChild.innerHTML = "Sign Up and Create Course!";
+    signUp2Btn.innerHTML = "Instructor Sign Up Now";
+  } else if (key == 1) {
+    redirectorSection.previousElementSibling.className = "";
+    signerMessage.firstElementChild.innerHTML = "Sign Up and Start Learning!";
+    signUp2Btn.innerHTML = "Student Sign Up Now";
+  }
+}
